284504df4a66a6c1f652ba7f4878af96e095d69c
Chris Wang <wenxuanwang86@gmail.com>
2024-06-20 05:13:51 -0700

    update CRUD

--- src/main/java/com/java/ems_backned/service/EmployeeService.java
+++ src/main/java/com/java/ems_backned/service/EmployeeService.java
@@ -2,8 +2,19 @@ package com.java.ems_backned.service;
 
 import com.java.ems_backned.dto.dto.EmployeeDto;
 
+import java.util.List;
+
 
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployeeDto);
+
+    void deleteEmployee(Long employeeId);
+
 }
--- src/main/java/com/java/ems_backned/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/java/ems_backned/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,15 @@ package com.java.ems_backned.service.impl;
 
 import com.java.ems_backned.dto.dto.EmployeeDto;
 import com.java.ems_backned.entity.Employee;
+import com.java.ems_backned.exception.ResourceNotFoundException;
 import com.java.ems_backned.mapper.EmployeeMapper;
 import com.java.ems_backned.repository.EmployeeRepository;
 import com.java.ems_backned.service.EmployeeService;
 import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +23,41 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee saveEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(saveEmployee);
     }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exists with given id : " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployeeDto) {
+            Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                              () -> new ResourceNotFoundException("Employee is not exists with given id : " + employeeId)
+                      );
+            employee.setFirstName(updateEmployeeDto.getFirstName());
+            employee.setLastName(updateEmployeeDto.getLastName());
+            employee.setEmail(updateEmployeeDto.getEmail());
+            Employee updateEmployeeObj = employeeRepository.save(employee);
+            return EmployeeMapper.mapToEmployeeDto(updateEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exists with given id : " + employeeId)
+        );
+        employeeRepository.deleteById(employeeId);
+    }
+
 
 }
