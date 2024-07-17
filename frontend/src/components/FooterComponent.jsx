260ad99d01369303a879025fc2bb7e2cbfa7000b
Chris Wang <wenxuanwang86@gmail.com>
2024-07-16 20:27:59 -0700

    united react

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span>All right reserved 2024 </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+
+
+const HeaderComponent = () => {
+  return (
+  <div>
+    <header>
+    <nav className="navbar navbar-dark bg-primary">
+        <a className="navbar-brand" href="https://github.com/chriswxwang">Employee Management System</a>
+    </nav>
+    </header>
+    </div>
+      
+  )
+}
+
+export default HeaderComponent
